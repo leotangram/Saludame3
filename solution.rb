8d1f3a3811e2fbbd4aa2458eefb3ff3662c8e2a5
Leonardo Omaña Cortés <leonardoomanacortes@MacBook-Pro-de-Leonardo.local>
2017-11-16 15:31:00 -0500

    saludame3

---
+++ solution.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+
+get '/' do
+	erb :"index"
+end 
+
+# get '/hola' do
+# 	if params[:nombre]
+# 		name = params[:nombre]
+# 		"<h1>!Hola #{name}!</h1>"
+# 	else
+# 		"<h1>!Hola #{name = "desconocido"}!"
+		
+# 	end
+# 	redirect '/hola'
+# end 
+
+# # get '/' do
+# # 	if params[:nombre]
+# # 		name = params[:nombre]
+# # 		"<h1>Hola #{name}!</h1>"
+# # 	else
+# # 		"<h1>Hola #{name = "desconocido"}!"
+		
+# # 	end
+# # 	redirect '/'
+# # end 
